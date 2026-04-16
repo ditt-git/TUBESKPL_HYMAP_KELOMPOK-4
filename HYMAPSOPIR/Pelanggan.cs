efae5bb35496c2b62a8e7188fba45a25a0f984bc
ditt-git <firdaus.radit2020@gmail.com>
2026-04-16 15:50:52 +0700

    penambahan class Enums dan Pelanggan

---
+++ HYMAPSOPIR/Pelanggan.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HYMAPSOPIR
+{
+    public class Pelanggan
+    {
+      
+        public string IdPelanggan {
+            get; 
+        }
+        public string NamaPelanggan { 
+            get; 
+        }
+        public string Alamat { 
+            get; 
+        }
+
+        public Armada Wilayah { 
+            get; 
+        }
+
+        public DateTime TanggalTerakhirKirim { 
+            get; 
+            private set; 
+        }
+
+        public Pelanggan(string id, string nama, string alamat, Armada wilayah, DateTime terakhirKirim)
+        {
+            IdPelanggan = id;
+            NamaPelanggan = nama;
+            Alamat = alamat;
+            TanggalTerakhirKirim = terakhirKirim;
+            Wilayah = wilayah;
+        }
+
+        // Method internal yang hanya dipanggil oleh sistem saat pengiriman selesai
+        public void UpdateTanggalPengirimanBerhasil(DateTime tanggalKirimBaru)
+        {
+            TanggalTerakhirKirim = tanggalKirimBaru;
+        }
+
+        // Menghitung jadwal berikutnya (Siklus 7 Hari)
+        public DateTime JadwalBerikutnya()
+        {
+            return TanggalTerakhirKirim.AddDays(7);
+        }
+    }
+}
+
