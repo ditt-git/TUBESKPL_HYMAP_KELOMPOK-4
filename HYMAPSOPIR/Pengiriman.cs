5a44fa3619a11ea5d5e3c7d17ca3af0bea402c7f
salmn-tkyk <salman.hafidzuddin05@gmail.com>
2026-04-19 20:58:19 +0700

    Penambahan Kelas Pengiriman dan PrioritasChecker

---
+++ HYMAPSOPIR/Pengiriman.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HYMAPSOPIR
+{
+    public class Pengiriman
+    {
+        public Pelanggan DataPelanggan { get; }
+        public StatusPengiriman StatusKirim { get; set; }
+        public StatusPembayaran StatusBayar { get; set; }
+        public PrioritasPengiriman Prioritas { get; }
+        public string BuktiFoto { get; set; }
+
+        public Pengiriman(Pelanggan pelanggan, DateTime tanggalHariIni)
+        {
+            DataPelanggan = pelanggan;
+            StatusKirim = StatusPengiriman.BelumTerkirim;
+            StatusBayar = StatusPembayaran.BelumBayar;
+            BuktiFoto = string.Empty;
+
+            // Kalkulasi prioritas saat pesanan di-generate hari ini
+            Prioritas = PrioritasChecker.HitungPrioritas(pelanggan.JadwalBerikutnya(), tanggalHariIni);
+        }
+    }
+}
--- HYMAPSOPIR/obj/Debug/net10.0/HYMAPSOPIR.AssemblyInfo.cs
+++ HYMAPSOPIR/obj/Debug/net10.0/HYMAPSOPIR.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HYMAPSOPIR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e71040cb4a6ec185a654b74661e1d8f576c7d281")]
 [assembly: System.Reflection.AssemblyProductAttribute("HYMAPSOPIR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HYMAPSOPIR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
