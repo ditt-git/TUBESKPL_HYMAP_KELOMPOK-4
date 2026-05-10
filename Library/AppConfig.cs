cb53876a04ce9e4d5f37a541ce6cf3fb49e2fad6
ditt-git <firdaus.radit2020@gmail.com>
2026-05-11 01:52:27 +0700

    penambahan runtime config

---
+++ Library/AppConfig.cs
@@ -0,0 +1,8 @@
+namespace HYMAPSOPIR
+{
+    public class AppConfig
+    {
+        public string AppName { get; set; } = "HYMAPSOPIR";
+        public string Version { get; set; } = "1.0.0";
+    }
+}
---
+++ Library/DataHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HYMAPSOPIR
+{
+    public static class DataHelper
+    {
+       
+        // IMPLEMENTASI GENERIC METHOD + GENERIC CONSTRAINT
+        // fungsi method ini mencari data apa pun (Sopir/Pelanggan) berdasarkan ID-nya
+        public static T CariBerdasarkanId<T, TId>(List<T> sumberData, TId idYangDicari) where T : EntitasDasar<TId>
+        {
+            if (sumberData == null) return null;
+
+            foreach (var item in sumberData)
+            {
+                // Mencocokkan ID secara dinamis
+                if (item.Id != null && item.Id.Equals(idYangDicari))
+                {
+                    return item;
+                }
+            }
+
+            return null; // Tidak ketemu
+        }
+    }
+}
